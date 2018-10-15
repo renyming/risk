5feafa81ccbd800fa7d6f7bfb886e801d60e0806
Lei Hao <leihao1313@gmail.com>
2018-10-15 02:16:33 -0400

    update model class

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,8 +1,17 @@
 package model;
 
+import java.util.List;
 import java.util.Observable;
 
 public class Model extends Observable {
+    private List<Continent> continents;
+
+    /**
+     * @return the continents list
+     */
+    public List<Continent> getContinents() {
+        return continents;
+    }
 
     /**
      * for test purpose
