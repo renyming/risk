ff9b39178236baa53d53d361edbd5290e8e64cb6
Lei Hao <leihao1313@gmail.com>
2018-10-15 00:42:32 -0400

    update MapValidator

--- src/validate/MapValidator.java
+++ src/validate/MapValidator.java
@@ -1,4 +1,15 @@
 package validate;
 
+import model.Continent;
+import model.Model;
+import model.Country;
+
+/**
+ * Map validator.
+ *
+ * @author Hao Lei
+ * @version 1.0.0
+ */
 public class MapValidator {
+
 }
