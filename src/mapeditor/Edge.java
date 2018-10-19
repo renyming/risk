3bd882b12564439e0c9856e01584313c54de618f
YM <renyming@gmail.com>
2018-10-18 23:41:33 -0400

    Clear map while loading new file, add exit()

--- src/mapeditor/Edge.java
+++ src/mapeditor/Edge.java
@@ -7,8 +7,8 @@ public class Edge extends Line {
     private Country p1, p2;
     public final static Color normalColor=Color.web("#7e6b8f");
     public final static Color deleteColor=Color.web("#f2e94e");
-    public final static int normalWidth=2;
-    public final static int deleteWidth=3;
+    public final static int normalWidth=3;
+    public final static int deleteWidth=4;
 
     public Edge(Country p1, Country p2){
         this.setStartX(p1.getLayoutX()+60);
