6078aefd3404f4e5c643cf2d43c5232f2e49d461
Lei Hao <leihao1313@gmail.com>
2018-11-07 17:12:25 -0500

    add CardType enum

---
+++ src/common/CardType.java
@@ -0,0 +1,12 @@
+package common;
+
+/**
+ * Card Enums for card exchange.
+ */
+public enum CardType {
+
+    /**
+     * The @INFANTRY @CAVALRY @ARTILLERY card enums.
+     */
+    INFANTRY, CAVALRY, ARTILLERY
+}
