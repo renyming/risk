51d4e1624005778da789cf15429485a91361e8f3
Lei Hao <leihao1313@gmail.com>
2018-11-09 05:20:33 -0500

    update cardmodel message using observer pattern

--- src/controller/CardController.java
+++ src/controller/CardController.java
@@ -3,10 +3,10 @@ package controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import common.CardType;
 import model.Card;
 import model.CardModel;
 import model.Model;
+import model.Player;
 import view.CardView;
 
 import javafx.stage.Stage;
@@ -31,6 +31,7 @@ public class CardController {
     private Model model;
     private CardView card;
     private MapController mapController;
+    private Player currentPlayer;
 
 
 
@@ -43,13 +44,10 @@ public class CardController {
     @FXML
     private void cancelCardView(ActionEvent event) {
         model.quitCards();
-        if(!CardModel.getInstance().getInvalidInfo().equals("you must exchange cards!")) {
+        if(CardModel.getInstance().readyToQuit()) {
             Stage stage = (Stage) cancelCardView.getScene().getWindow();
             stage.close();
-        }else{
-            textToShow.setText("more than 5.");
         }
-
     }
 
 
@@ -57,35 +55,24 @@ public class CardController {
     private void checkTrade(ActionEvent event) {
         trade.setDisable(false);
         textToShow.setText(null);
-        List<Card> selectedCards = CardModel.getInstance().retrieveSelectedCardsFromCheckbox(
-                CardModel.getInstance().getCurrentPlayer().getPlayerCardList(),cbs);
-        System.out.println("num slelecd"+ selectedCards.size());
+        currentPlayer = CardModel.getInstance().getCurrentPlayer();
+        List<Card> selectedCards = CardModel.getInstance().retrieveSelectedCardsFromCheckbox
+                (currentPlayer.getPlayerCardList(),cbs);
         if (selectedCards.size() == 3) {
-
-            model.trade((ArrayList<Card>) selectedCards);
-            /*boolean flag = cardModel.checkTradePossible(selectedCards);
-
-            if (flag) {
-                cardModel.setCardsExchangeable(selectedCards);
-                Stage stage = (Stage) trade.getScene().getWindow();
-                stage.close();
-            } else {
-                textToShow.setText("Invalid Combination.");
-                trade.setDisable(false);
-                return;
-            }*/
-        } else {
-            textToShow.setText("Select Only 3 Card");
-            return;
+        model.trade((ArrayList<Card>) selectedCards);
         }
-
     }
-
 
     public void autoInitializeController() {
         cardVbox.getChildren().clear();
-        currentPlayerName.setText("Cards of " + CardModel.getInstance().getCurrentPlayer().getName());
-        playerCards = CardModel.getInstance().getCurrentPlayer().getPlayerCardList();
+        currentPlayer = CardModel.getInstance().getCurrentPlayer();
+        currentPlayerName.setText("All Cards Of Player : " + currentPlayer.getName());
+        textToShow.setStyle("-fx-text-fill: red");
+        if(CardModel.getInstance().finishExchange()){
+            textToShow.setStyle("-fx-text-fill: green");
+        }
+        textToShow.setText(CardModel.getInstance().getInvalidInfo());
+        playerCards = currentPlayer.getPlayerCardList();
 
         if (playerCards.size() < 3) {
             trade.setDisable(true);
@@ -93,14 +80,11 @@ public class CardController {
             trade.setDisable(false);
         }
         loadAllCards();
-
-        //tryCardTrade();
     }
 
 
     public void loadAllCards() {
         int numberOfCards = playerCards.size();
-        System.out.println("number of current palyer cards "+numberOfCards);
         cbs = new CheckBox[numberOfCards];
         for (int i = 0; i < numberOfCards; i++) {
             cbs[i] = new CheckBox(
@@ -109,12 +93,4 @@ public class CardController {
         cardVbox.getChildren().addAll(cbs);
     }
 
-    public void tryCardTrade() {
-        playerCards = Model.getCurrentPlayer().getPlayerCardList();
-        //List<Card> cardModel = cardModel.getValidCardComibination(playerCards);
-        List<model.Card> cardss = playerCards;
-        if (cardss != null && cardss.size() == 3) {
-            CardModel.getInstance().setCardsExchangeable(cardss);
-        }
-    }
 }
