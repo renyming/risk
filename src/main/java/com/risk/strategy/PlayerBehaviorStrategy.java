355581a20ac2afa92405ccd3277d96c3f4eeae73
Zhijing Ling <ivyling063@gmail.com>
2018-11-26 19:34:24 -0500

    Refactor: 1. Put general method back in player class; 2. Redefine the interface of PlayerBehaviorStrategy(including reinforcement, attack, moveArmy and fortification four method)
    This refactor passed the previous unit test.

--- src/main/java/com/risk/strategy/PlayerBehaviorStrategy.java
+++ src/main/java/com/risk/strategy/PlayerBehaviorStrategy.java
@@ -5,13 +5,11 @@ import com.risk.model.Country;
 public interface PlayerBehaviorStrategy {
     void reinforcement( );
 
-    void allocateArmy(Country country, boolean disable);
+//    void allocateArmy(Country country, boolean disable);
 
     void attack(Country attacker, String attackerNum, Country defender, String defenderNum, boolean isAllOut);
 
-    void moveArmy(String num);
-
-    boolean isAttackPossible();
+    void moveArmy(Country attacker, int attackerDiceNum, Country defender, int defenderDiceNum, String num);
 
     void fortification(Country source, Country target, int armyNumber);
 
