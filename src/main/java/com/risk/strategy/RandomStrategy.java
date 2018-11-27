754c25036030eca8742c956ca4eeb5d321b41d18
YM <renyming@gmail.com>
2018-11-26 21:58:41 -0500

    Implement strategy interface (empty functions)

--- src/main/java/com/risk/strategy/RandomStrategy.java
+++ src/main/java/com/risk/strategy/RandomStrategy.java
@@ -1,16 +1,28 @@
-//package com.risk.strategy;
-//
-//import com.risk.model.Country;
-//import com.risk.model.Player;
-//
-//public class RandomStrategy implements PlayerBehaviorStrategy {
-//    @Override
-//    public void reinforcement(Player player) {
-//
-//    }
-//
-//    @Override
-//    public void attack(Country attacker, String attackerNum, Country defender, String defenderNum, boolean isAllOut) {
-//
-//    }
-//}
+package com.risk.strategy;
+
+import com.risk.model.Country;
+import com.risk.model.Player;
+
+public class RandomStrategy implements PlayerBehaviorStrategy {
+
+    @Override
+    public void reinforcement() {
+
+    }
+
+    @Override
+    public void attack(Country attacker, String attackerNum, Country defender, String defenderNum, boolean isAllOut) {
+
+    }
+
+    @Override
+    public void moveArmy(String num) {
+
+    }
+
+    @Override
+    public void fortification(Country source, Country target, int armyNumber) {
+
+    }
+
+}
