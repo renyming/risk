3b655406d6446393f23ec3d486dea7347f781c75
YM <renyming@gmail.com>
2018-11-28 01:10:11 -0500

    Test for random reinforcement operation

--- src/test/java/com/risk/strategy/RandomStrategyTest.java
+++ src/test/java/com/risk/strategy/RandomStrategyTest.java
@@ -10,6 +10,7 @@ import org.junit.*;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class RandomStrategyTest {
 
@@ -94,11 +95,19 @@ public class RandomStrategyTest {
         // china = 4, thailand = 1
         // addedArmies = 3
         defender.reinforcement();
-        assertEquals(7, china.getArmies());
-        assertEquals(1, thailand.getArmies());
-        assertEquals(0, defender.getArmies());
+        assertTrue(china.getArmies()==7 || thailand.getArmies()==4);
+        assertEquals(defender.getArmies(),0);
         assertEquals(8, defender.getTotalStrength());
         Assert.assertEquals(Action.Show_Next_Phase_Button, Phase.getInstance().getActionResult());
+
+        // original totalStrength = 10
+        // singapore = 1, canada = 1, usa = 8
+        // addedArmies = 7
+        player.reinforcement();
+        assertTrue(singapore.getArmies()==8 || canada.getArmies()==8 || usa.getArmies()==15);
+        assertEquals(player.getArmies(),0);
+        assertEquals(17, player.getTotalStrength());
+        assertEquals(Action.Show_Next_Phase_Button, Phase.getInstance().getActionResult());
     }
 
     @Test
