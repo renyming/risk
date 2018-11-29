f18abe3169edf6044bd0d5df0c70d5a2a12c8a36
YM <renyming@gmail.com>
2018-11-29 12:42:53 -0500

    Changed delete country button style

--- src/main/java/com/risk/mapeditor/CountryController.java
+++ src/main/java/com/risk/mapeditor/CountryController.java
@@ -9,9 +9,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 /**
@@ -31,7 +34,7 @@ public class CountryController {
     @FXML
     AnchorPane right_pane;
     @FXML
-    Button btnRemove;
+    Label btnRemove;
 
     /**
      * Initialization
@@ -54,24 +57,30 @@ public class CountryController {
         });
 
         // remove button listener
-        btnRemove.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                ArrayList<Edge> edgeList=new ArrayList<>(country.getEdgeList());
-                for (Edge line:edgeList){
-                    Country p1=line.getP1();
-                    Country p2=line.getP2();
-                    p1.removeAdjCountry(p2);
-                    p2.removeAdjCountry(p1);
-                    p1.removeEdge(line);
-                    p2.removeEdge(line);
-                    AnchorPane draw_pane=(AnchorPane) line.getParent();
-                    draw_pane.getChildren().remove(line);
-                }
-                AnchorPane draw_pane=(AnchorPane) country.getParent();
-                draw_pane.getChildren().remove(country);
-                event.consume();
+        btnRemove.setOnMouseClicked(event ->  {
+            ArrayList<Edge> edgeList=new ArrayList<>(country.getEdgeList());
+            for (Edge line:edgeList){
+                Country p1=line.getP1();
+                Country p2=line.getP2();
+                p1.removeAdjCountry(p2);
+                p2.removeAdjCountry(p1);
+                p1.removeEdge(line);
+                p2.removeEdge(line);
+                AnchorPane draw_pane=(AnchorPane) line.getParent();
+                draw_pane.getChildren().remove(line);
             }
+            AnchorPane draw_pane=(AnchorPane) country.getParent();
+            draw_pane.getChildren().remove(country);
+            event.consume();
+        });
+
+        btnRemove.setOnMouseEntered(event -> {
+            btnRemove.setTextFill(Edge.deleteColor);
+        });
+
+        btnRemove.setOnMouseExited(event -> {
+
+            btnRemove.setTextFill(Color.BLACK);
         });
 
         //Choice box listener
@@ -81,6 +90,11 @@ public class CountryController {
                 country.setContinent(newValue);
                 System.out.println(newValue);
             }
+        });
+
+        //Country title listener
+        lblCountry.setOnMouseClicked(event -> {
+            lblCountry.setStyle("-fx-background-color: #FFFFFF; -fx-background-radius: 10 0 0 0");
         });
 
 
